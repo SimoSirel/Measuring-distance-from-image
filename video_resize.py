ee492e96ab3e77993dedd9f583489768398ba3db
Sille Habakukk <sille.habakukk@gmail.com>
2020-12-08 18:48:20 +0200

    Fixes to resize.

--- video_resize.py
+++ video_resize.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     output_video_filepath = params.get('-out', 'data/smaller_video.avi')
     left_video_filepath = params.get('-out', 'data/smaller_left_video.avi')
     right_video_filepath = params.get('-out', 'data/smaller_right_video.avi')
-    fps_reduction = params.get('-fps_reduce', 2)
+    fps_reduction = params.get('-fps_reduce', 3)
     size_reduction = params.get('-size_reduce', 4)
 
     # Initialize VideoCapture
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     fps = int(input_video.get(cv2.CAP_PROP_FPS)//fps_reduction)
     width = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH)//size_reduction)  # float
     height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT)//size_reduction)  # float
-    total_frames = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
+    #total_frames = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
 
     input_video.release()
 
@@ -40,17 +40,18 @@ if __name__ == '__main__':
     # Initialize output video
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     output_video = cv2.VideoWriter(output_video_filepath, fourcc, fps, (width, height))
-    left_video = cv2.VideoWriter(left_video_filepath, fourcc, fps, (width, height))
-    right_video = cv2.VideoWriter(right_video_filepath, fourcc, fps, (width, height))
+    left_video = cv2.VideoWriter(left_video_filepath, fourcc, fps, (width//2, height))
+    right_video = cv2.VideoWriter(right_video_filepath, fourcc, fps, (width//2, height))
 
     input_video = cv2.VideoCapture(input_video_filepath)
     frame_number = 0
     while input_video.isOpened():
         ret, frame = input_video.read()
         frame_number += 1
-
+        #print(frame_number)
         if (frame_number%fps_reduction==0) :
             if frame is not None:
+                #print("lisan kaadri")
                 small_frame = cv2.resize(frame,(width, height))
                 left_frame = small_frame[0:height , 0:width//2]
                 right_frame = small_frame[0:height , 0:width//2]
@@ -58,13 +59,11 @@ if __name__ == '__main__':
                 output_video.write(small_frame)
                 left_video.write(left_frame)
                 right_video.write(right_frame)
-                cv2.imshow('frame', small_frame)
 
-        else:
-            break
+            else:
+                break
 
     input_video.release()
     output_video.release()
     left_video.release()
     right_video.release()
-    cv2.destroyAllWindows()
